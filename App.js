152404373f6cced8782dd26a2db08f56ae0f2f2a
Luis Carlin <luiscarlin@gmail.com>
2019-08-21 06:23:21 -0400

    make it more tight

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import './app.css'
+import React from "react";
+import "./app.scss";
 
 function App() {
   return (
-  <div className='app'>
-    <p>Your content here</p>
-  </div>
-  )
+    <div className="app">
+      <p>Your content here</p>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-import './index.css'
-import 'normalize.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "normalize.css";
 
-const rootElement = document.getElementById('root')
+const rootElement = document.getElementById("root");
 
-ReactDOM.render(<App/>, rootElement)
+ReactDOM.render(<App />, rootElement);
